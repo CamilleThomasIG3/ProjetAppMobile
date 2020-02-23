eb9503e2938f47e2572284bbacae9f36ab015642
CamilleThomas <camille.thomas@etu.umontpellier.fr>
2020-02-23 17:35:09 +0000

    Initial Commit

---
+++ ProjetAppMobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjetAppMobile
+//
+//  Created by user165000 on 2/23/20.
+//  Copyright © 2020 BourratSanchezThomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
