0bfc6af4ee8547186686259007de30cbfcad627a
CamilleThomasIG3 <camille.thomas@etu.umontpellier.fr>
2020-03-17 14:36:25 +0100

    prise en main du serveur

--- serverNode/models/Remark.js
+++ serverNode/models/Remark.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RemarkSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
     content: {
         type: String,
         required: true
