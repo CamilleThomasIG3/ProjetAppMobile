ad09f4a5bd75e0e9785f5959a82c8e4dfc74addd
CamilleThomasIG3 <camille.thomas@etu.umontpellier.fr>
2020-03-30 21:01:20 +0200

    prepare for deployment

--- serverNode/server.js
+++ serverNode/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
-
+const path = require('path')
 
 const items = require('./routes/api/items');
 const remarks = require('./routes/api/remarks');
@@ -12,7 +12,7 @@ const users = require('./routes/api/users');
 const app = express();
 
 //Bodyparser middleware 
-app.use(express.json());
+app.use(express.json({extended: false}));
 
 //mongo config
 const db = config.get('mongoURI');
@@ -27,6 +27,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/remarks', require('./routes/api/remarks'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/items', require('./routes/api/items'));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 
